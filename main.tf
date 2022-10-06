45d3c27554050e02584857a2dde6e0fcc4cc44e3
Chris Doty <dotyc@civicinc.com>
2022-10-05 21:33:53 -0500

    fix: iterim commit

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ provider "aws" {
   s3_use_path_style           = true
 
   endpoints {
+    acm            = "http://localhost:4566"
     apigatewayv2   = "http://localhost:4566"
     apigateway     = "http://localhost:4566"
     cloudformation = "http://localhost:4566"
@@ -36,11 +37,6 @@ provider "aws" {
     sts            = "http://localhost:4566"
     ec2            = "http://localhost:4566"
   }
-}
-
-resource "aws_s3_bucket" "b" {
-  bucket = "localstack-s3-dynamo"
-  tags   = var.tags
 }
 
 resource "aws_dynamodb_table" "countries_table" {
@@ -132,3 +128,92 @@ resource "aws_route53_record" "api" {
   }
 }
 */
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "testing.tensouthtech.com"
+  validation_method = "EMAIL"
+
+  validation_option {
+    domain_name       = "testing.tensouthtech.com"
+    validation_domain = "tensouthtech.com"
+  }
+}
+
+resource "aws_s3_bucket" "cdn" {
+  bucket = "localstack-cdn"
+  tags   = var.tags
+}
+
+resource "aws_s3_bucket" "cdnlog" {
+  bucket = "localstack-cdn-log"
+  tags   = var.tags
+}
+
+module "cdn" {
+  source = "terraform-aws-modules/cloudfront/aws"
+
+  aliases = ["cdn.tensouthtech.com"]
+
+  comment             = "this is my cloudfront"
+  enabled             = true
+  is_ipv6_enabled     = true
+  price_class         = "PriceClass_All"
+  retain_on_delete    = false
+  wait_for_deployment = false
+
+  create_origin_access_identity = true
+  origin_access_identities = {
+    s3_bucket_one = aws_s3_bucket.cdn.bucket
+  }
+
+  logging_config = {
+    bucket = "${aws_s3_bucket.cdnlog.bucket}.s3.amazonaws.com"
+  }
+
+  origin = {
+    something = {
+      domain_name = aws_acm_certificate.cert.domain_name
+      custom_origin_config = {
+        http_port              = 80
+        https_port             = 443
+        origin_protocol_policy = "match-viewer"
+        origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      }
+    }
+
+    s3_one = {
+      domain_name = "${aws_s3_bucket.cdn.bucket}.s3.amazonaws.com"
+      s3_origin_config = {
+        origin_access_identity = aws_s3_bucket.cdn.bucket
+      }
+    }
+  }
+
+  default_cache_behavior = {
+    target_origin_id           = "something"
+    viewer_protocol_policy     = "allow-all"
+
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD"]
+    compress        = true
+    query_string    = true
+  }
+
+  ordered_cache_behavior = [
+    {
+      path_pattern           = "/static/*"
+      target_origin_id       = "s3_one"
+      viewer_protocol_policy = "redirect-to-https"
+
+      allowed_methods = ["GET", "HEAD", "OPTIONS"]
+      cached_methods  = ["GET", "HEAD"]
+      compress        = true
+      query_string    = true
+    }
+  ]
+
+  viewer_certificate = {
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method  = "sni-only"
+  }
+}
